47bc049b91a90ff2ca89dd0d3a94057e15202ef3
Sophie Louise Feith <sfeith@student.codam.nl>
2021-06-21 16:04:29 +0200

    server

--- networking/Servers/SimpleServer.cpp
+++ networking/Servers/SimpleServer.cpp
@@ -1,1 +1,9 @@
-#include SimpleServer.hpp"
+#include "SimpleServer.hpp"
+
+
+HDE::SimpleServer::SimpleServer(int domain, int service, int protocol, int port, u_long interface, int bklg){
+
+    /* new pointer watch out for memory leak*/
+    socket = new listentingSocket(domain, service, protocol, port, interface, bklg);
+    //delete socket;
+}
--- networking/Servers/SimpleServer.hpp
+++ networking/Servers/SimpleServer.hpp
@@ -6,4 +6,19 @@
 
 #include "../hdelibc_networking.hpp"
 
+namespace HDE{
+    
+class SimpleServer{
+
+
+
+    private:
+    listentingSocket * socket;
+
+    public:
+    SimpleServer(int domain, int service, int protocol, int port, u_long interface, int bklg);
+    }
+
+};
+
 #endif
