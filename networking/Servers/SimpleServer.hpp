48b200d514de22d6d7216a0df42b73245294cb26
Sophie Louise Feith <sfeith@student.codam.nl>
2021-06-21 16:07:42 +0200

    server part

--- networking/Servers/SimpleServer.hpp
+++ networking/Servers/SimpleServer.hpp
@@ -14,11 +14,13 @@ class SimpleServer{
 
     private:
     listentingSocket * socket;
-
+    virtual void accepter()= 0;
+    virtual void handeler()= 0;
+    virtual void responder()= 0;
     public:
     SimpleServer(int domain, int service, int protocol, int port, u_long interface, int bklg);
-    }
-
 };
+
+}
 
 #endif
