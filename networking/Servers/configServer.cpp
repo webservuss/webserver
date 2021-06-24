b6df36a5f8fc883a713edcdf1b224762cf2d9ab9
Amber Van dam <avan-dam@f1r1s3.codam.nl>
2021-06-24 13:50:57 +0200

    starting the config parser

---
+++ networking/Servers/configServer.cpp
@@ -0,0 +1,72 @@
+#include "SimpleServer.hpp"
+
+//  setters
+void		HDE::configServer_hpp::set_server_name(std::string srv_name)
+{
+    _srv_name = server_name;
+}
+
+void		HDE::configServer_hpp::set_port(int prt)
+{
+    _port = prt;
+}
+
+void		HDE::configServer_hpp::set_host(std::string hst)
+{
+    hst = _host;
+}
+
+void		HDE::configServer_hpp::set_error_page(std::string err_pg)
+{
+    _error_page = err_pg;
+}
+
+void		HDE::configServer_hpp::set_auto_index(int ato_ndx)
+{
+    _auto_index = ato_ndx;
+}
+
+void 		HDE::configServer_hpp::set_root(std::string rt)
+{
+    _root = rt;
+}
+
+void 		HDE::configServer_hpp::set_index(std::string ndx)
+{
+    _index = ndx;
+}
+
+//  getters
+std::string		HDE::configServer_hpp::set_server_name()
+{
+    return _server_name;
+}
+
+int				HDE::configServer_hpp::set_port()
+{
+    return _port;
+}
+
+std::string		HDE::configServer_hpp::set_host()
+{
+    return _host;
+}
+
+std::string		HDE::configServer_hpp::set_error_page()
+{
+    return _error_page;
+}
+
+int				HDE::configServer_hpp::set_auto_index()
+{
+    return _auto_index;
+}
+std::string 	HDE::configServer_hpp::set_root()
+{
+    return root;
+}
+
+std::string 	HDE::configServer_hpp::set_index()
+{
+    return _index;
+}
--- networking/Servers/testserver.cpp
+++ networking/Servers/testserver.cpp
@@ -25,15 +25,6 @@ void HDE::TestServer::handeler()
 void HDE::TestServer::responder()
 {
     char  *hello = "hello from server";
-        // char * msg= "GET / HTTP/1.1\r\n"
-        //         "Host: 192.241.213.46:6880\r\n"
-        //         "Upgrade-Insecure-Requests: 1\r\n"
-        //         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-        //         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8\r\n"
-        //         "Accept-Language: en-us\r\n"
-        //         "Accept-Encoding: gzip, deflate\r\n"
-        //         "Connection: keep-alive\r\n\r\n";
-    // write(new_socket, msg, strlen(msg));
     write(new_socket, hello, strlen(hello));
     close(new_socket);
 }
