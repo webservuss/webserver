37ce24e3eaac9b9ef3e6f7f16fffb0f9773f9aff
sophielouisefeith <57356744+sophielouisefeith@users.noreply.github.com>
2021-08-24 22:54:51 +0200

    configs

--- networking/Servers/test.cpp
+++ networking/Servers/test.cpp
@@ -1,11 +1,56 @@
 #include "select_server.hpp"
 #include "parser_conf.hpp"
+#include "test.hpp"
+#include "test.hpp"
+#include <dirent.h>
 
-int main()
+
+int main(int argc, char **argv)
 {
+
+
+
+
+
+
+
+
+
+
+
+
     // implement so it can handle different configs.
-	const char *path = "configs/server.conf";
-	HTTP::parse_conf ex(path);
+   
+    std::string  pathname;
+    for(int i= 1; i<argc; i++) 
+        pathname = (argv[i]);
+    if( argc == 1)
+    {
+        std::cout << "choos a config in configs"<<std::endl;
+        exit(10);
+    }
+	HTTP::parse_conf ex(pathname.c_str());
     std::vector<HTTP::t_server> parser_servers = ex.get_server();
     HTTP::select_server t(ex.get_ports(), parser_servers) ;
+}
+
+std::vector<std::string>config()
+{
+    const char * record_dir_path = "configs";
+    std::vector<std::string> files;
+    struct dirent *entry;
+	DIR *dir = opendir(record_dir_path);
+
+	if (dir == NULL) 
+	{
+	  return files;
+	}
+	while ((entry = readdir(dir)) != NULL) 
+	{
+        std::cout << "open>?"<< std::endl;
+		files.push_back(entry->d_name);
+	}
+	closedir(dir);
+	
+	return(files);
 }
