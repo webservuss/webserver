b72752d8a6b1711c4e60257ffc8c150636f1b37a
Ruben <rvan-sch@student.codam.nl>
2021-09-22 16:24:37 +0200

    autoindex

---
+++ bin/auto_index.php
@@ -0,0 +1,72 @@
+<?PHP
+  function getFileList($dir)
+  {
+    // array to hold return value
+    $retval = [];
+
+    // add trailing slash if missing
+    if(substr($dir, -1) != "/") {
+      $dir .= "/";
+    }
+
+    // open pointer to directory and read list of files
+    $d = @dir($dir) or die("getFileList: Failed opening directory {$dir} for reading");
+    while(FALSE !== ($entry = $d->read())) {
+      // skip hidden files
+      if($entry{0} == ".") continue;
+      if(is_dir("{$dir}{$entry}")) {
+        $retval[] = [
+          'name' => "{$entry}/",
+          'type' => filetype("{$dir}{$entry}"),
+          'size' => 0,
+          'lastmod' => filemtime("{$dir}{$entry}")
+        ];
+      } elseif(is_readable("{$dir}{$entry}")) {
+        $retval[] = [
+          'name' => "{$entry}",
+          'type' => mime_content_type("{$dir}{$entry}"),
+          'size' => filesize("{$dir}{$entry}"),
+          'lastmod' => filemtime("{$dir}{$entry}")
+        ];
+      }
+    }
+    $d->close();
+
+    return $retval;
+  }
+?>
+
+<?PHP
+  // list files in the current directory
+//   $dirlist = getFileList(".");
+//   $dirlist = getFileList("./");
+
+  // a subdirectory of the current directory called images
+  //$dirlist = getFileList("images");
+  //$dirlist = getFileList("images/");
+  //$dirlist = getFileList("./images");
+  //$dirlist = getFileList("./images/");
+
+  // using an absolute path
+ $dirlist = getFileList("{$_SERVER['AUTO_INDEX']}");
+ // $dirlist = getFileList("{$_SERVER['DOCUMENT_ROOT']}/images/");
+?>
+
+<?PHP
+  // output file list in HTML TABLE format
+  echo "<table border=\"1\">\n";
+  echo "<thead>\n";
+  echo "<tr><th>Name</th><th>Type</th><th>Size</th><th>Last Modified</th></tr>\n";
+  echo "</thead>\n";
+  echo "<tbody>\n";
+  foreach($dirlist as $file) {
+    echo "<tr>\n";
+    echo "<td>{$file['name']}</td>\n";
+    echo "<td>{$file['type']}</td>\n";
+    echo "<td>{$file['size']}</td>\n";
+    echo "<td>",date('r', $file['lastmod']),"</td>\n";
+    echo "</tr>\n";
+  }
+  echo "</tbody>\n";
+  echo "</table>\n\n";
+?>
